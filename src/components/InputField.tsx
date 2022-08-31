945507e803bdeaafe4a09cd1daf1d2ba09972bcc
vlad <vladpuschiulescu@gmail.com>
2022-08-31 11:13:19 +0300

    Handle input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,29 @@
 import "./styles.css"
-import React from "react";
+import React, {useRef} from "react";
 
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent) => void
 }
 
-const InputField = ({ todo, setTodo }:Props) => {
+const InputField = ({ todo, setTodo, handleAdd }:Props) => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
     return (
-        <form className={"input"}>
-            <input type="input"
-                   value={todo}
-                   onChange={
-                        (e) => setTodo(e.target.value)}
-                   placeholder="Enter a task" className="input__box"/>
+        <form className={"input"} onSubmit={(e) => {
+            handleAdd(e)
+            inputRef.current?.blur()
+        }}>
+            <input
+                type="input"
+                ref={inputRef}
+                value={todo}
+                onChange={
+                    (e) => setTodo(e.target.value)}
+                placeholder="Enter a task" className="input__box"
+            />
             <button className={"input_submit"} type={"submit"}>Go</button>
         </form>
     );
